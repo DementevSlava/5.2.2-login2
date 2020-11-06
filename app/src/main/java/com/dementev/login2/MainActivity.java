6d3fe668b266673fd131ea87e8bfdd14c956143e
Dementev Slava <kursant344@gmail.com>
2020-11-06 15:44:43 +0300

    add SharedPreference

--- app/src/main/java/com/dementev/login2/MainActivity.java
+++ app/src/main/java/com/dementev/login2/MainActivity.java
@@ -2,9 +2,13 @@ package com.dementev.login2;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -19,8 +23,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String LOGIN_FILE = "login.txt";
-    private static final String PASS_FILE = "pass.txt";
+
     private static final String USER_FILE = "user.txt";
     public static final int REQUEST_CODE_PERMISSION_WRITE_STORAGE = 11;
 
@@ -28,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText enterPass;
     private Button loginBtn;
     private Button regBtn;
+    private CheckBox checkBox;
+    private SharedPreferences sharedPrefCheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +56,8 @@ public class MainActivity extends AppCompatActivity {
         enterPass = findViewById(R.id.enterPass);
         loginBtn = findViewById(R.id.loginBtn);
         regBtn = findViewById(R.id.regBtn);
+        checkBox = findViewById(R.id.checkboxStorage);
+        sharedPrefCheck = getSharedPreferences("CheckMemory", MODE_PRIVATE);
 
 
         regBtn.setOnClickListener(v -> {
@@ -63,22 +70,32 @@ public class MainActivity extends AppCompatActivity {
         loginBtn.setOnClickListener(v -> {
             String editLogin = enterLogin.getText().toString();
             String editPass = enterPass.getText().toString();
-            String
+            String[] user = readFromInternalStorage();
 
-            if (editLogin.equals(saveLogin) && editPass.equals(savePass)){
+            if (editLogin.equals(user[0]) && editPass.equals(user[1])){
                 Toast.makeText(this, "Данные верны", Toast.LENGTH_SHORT).show();
             } else {
                 Toast.makeText(this, "Неверные данные", Toast.LENGTH_SHORT).show();
             }
         });
+
+        checkBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            SharedPreferences.Editor editor = sharedPrefCheck.edit();
+            if (isChecked) {
+                editor.putString("memory", "external");
+            } else {
+                editor.putString("memory", "internal");
+            }
+            editor.apply();
+
+        });
     }
 
-    private String[] readFromInternalStorage(String fileName) {
+    private String[] readFromInternalStorage() {
         BufferedReader reader = null;
         try {
-            reader = new BufferedReader(new InputStreamReader(openFileInput(fileName)));
-             String[] strings = reader.readLine().split(";");
-             return strings;
+            reader = new BufferedReader(new InputStreamReader(openFileInput(USER_FILE)));
+            return reader.readLine().split("\n\n");
         } catch (IOException e) {
             e.printStackTrace();
             return null;
@@ -98,7 +115,7 @@ public class MainActivity extends AppCompatActivity {
         BufferedWriter writer = null;
         try {
             writer = new BufferedWriter(new OutputStreamWriter(openFileOutput(USER_FILE, Context.MODE_PRIVATE)));
-            writer.write(String.format("%s;%s", editLogin, editPass));
+            writer.write(String.format("%s\n\n%s", editLogin, editPass));
             Toast.makeText(this, "Данные сохранены", Toast.LENGTH_SHORT).show();
         } catch (IOException e) {
             e.printStackTrace();
@@ -112,6 +129,12 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
+    }
+
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        return Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state);
     }
 
 
